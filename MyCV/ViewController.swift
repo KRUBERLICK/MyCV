15188ea0f8f81fcbf4b1de966af6159c84e04a7f
Daniel Ilchyshyn <kruberlick@gmail.com>
2016-04-27 20:45:57 +0300

    Initial commit

--- MyCV/ViewController.swift
+++ MyCV/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    
+    //MARK: - Properties
+    
+    let regionRadius: CLLocationDistance = 3000
+    
+    
+    
+    //MARK: - MapView setup
+    
+    //center map view over the given location
+    func centerMapOnLocation(location: CLLocation) {
+        
+        //set square region
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        
+        //set region to map view
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    
+    //MARK: - Supreclass Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //kiev coordinates
+        let initialLocation = CLLocation(latitude: 50.4501, longitude: 30.5234)
+        
+        //setup map view
+        centerMapOnLocation(initialLocation)
+        
+        //make user photo image round
+        photoImageView.layer.cornerRadius = 160
+        photoImageView.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
