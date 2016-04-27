66aa9502fca513c67c83418f18f5c53bebd1c0f0
Daniel Ilchyshyn <kruberlick@gmail.com>
2016-04-27 18:42:06 +0300

    Initial Commit

---
+++ MyCV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Данил Ильчишин on 4/27/16.
+//  Copyright © 2016 KRUBERLICK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
